e227638c90f02ec9495344858956b6c19d54c7f5
Neamul <neamul.cse6.bu@gmail.com>
2024-06-09 04:42:43 +0600

    edit

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # Define the absolute path to the directory where collected static files will be stored.
-STATIC_ROOT = ('/core/static/img/')
+STATIC_ROOT = ('core/static/img/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
